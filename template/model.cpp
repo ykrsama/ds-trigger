930d675d7bada6df70658d2ec826827af4336138
Xuliang Zhu <xuliangz@sjtu.edu.cn>
2025-12-07 19:14:57 +0800

    fix II

--- template/model.cpp
+++ template/model.cpp
@@ -68,12 +68,12 @@ void GroupNorm3D(float input_data[BATCH_SIZE][T_IN_CHANNELS][T_INPUT_DEPTH][T_IN
             for (int height = 0; height < T_INPUT_HEIGHT; height++) {
                 StatWidth:
                 for (int width = 0; width < T_INPUT_WIDTH; width++) {
-                    #pragma HLS pipeline II=1
                     StatGroup:
                     for (int g = 0; g < NUM_GROUPS; g++) {
-                        #pragma HLS unroll
+                        #pragma HLS pipeline II=1
                         StatChan:
                         for (int ch = 0; ch < CHANNELS_PER_GROUP; ch++) {
+                            #pragma HLS unroll
                             int channel_idx = g * CHANNELS_PER_GROUP + ch;
                             float value = gn_buffer[batch][channel_idx][depth][height][width];
                             // Accumulate statistics
